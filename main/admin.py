5f50d4aa516cdadb04203d59bf324c4085f55a7b
Samir Mejia <samir04m.dev@gmail.com>
2022-03-21 23:18:17 -0500

    Django Import Export

--- main/admin.py
+++ main/admin.py
@@ -1,24 +1,29 @@
+from import_export.admin import ImportExportModelAdmin
 from django.contrib import admin
 from .models import *
 
-class RoomAdmin(admin.ModelAdmin):
+class RoomAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     search_fields = ['name']
     list_display = ('name',)
 
-class ContainerAdmin(admin.ModelAdmin):
+class ContainerAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     search_fields = ['name','room__name']
     list_display = ('name','room')
 
-class SubcontainerAdmin(admin.ModelAdmin):
+class SubcontainerAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     search_fields = ['name','container__name']
     list_display = ('name','container',)
 
-class ObjectAdmin(admin.ModelAdmin):
+class ObjectAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     search_fields = ['name','subcontainer__name']
     list_display = ('name','subcontainer','public',)
+
+class ViewLogAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    search_fields = ['user__username']
+    list_display = ('id','user','object','last_seen')
 
 admin.site.register(Room, RoomAdmin)
 admin.site.register(Container, ContainerAdmin)
 admin.site.register(Subcontainer, SubcontainerAdmin)
 admin.site.register(Object, ObjectAdmin)
-admin.site.register(ViewLog)
+admin.site.register(ViewLog, ViewLogAdmin)
