1c1945daacfcce76096a727a2c065451cf787562
Samir Mejia <samir04m.dev@gmail.com>
2022-03-22 21:48:29 -0500

    Install django-cleanup automatically delete images

--- buscador/settings.py
+++ buscador/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_cleanup.apps.CleanupConfig',
     'import_export',
     'main.apps.MainConfig',
     'myadmin.apps.MyadminConfig',
