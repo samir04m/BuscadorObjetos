9ce370c3f9aa8760c92f7430c519c849791235ed
Samir Mejia <samir04m.dev@gmail.com>
2025-04-18 02:36:21 -0500

    Implementación de paginación

--- main/views.py
+++ main/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.models import User
+from django.core.paginator import Paginator
 from datetime import datetime
 from django.db.models import Q
 from functools import reduce
@@ -21,21 +22,47 @@ def Logout(request):
 
 @login_required
 def Home(request):
-    context = {}
-    if request.method == 'POST':
-        query = request.POST.get('query')
-        words = query.split(' ')
+    query = request.GET.get('query', '')
+    context = {'query': query}
+
+    if query:
+        words = query.split()
         wordsToSearch = reduce(operator.or_, (Q(name__icontains=w) for w in words))
-        
+
         if request.user.is_superuser:
-            # results = Object.objects.filter(Q(name__icontains=query))
             results = Object.objects.filter(wordsToSearch)
         else:
-            results = Object.objects.filter( Q(public=1) & Q(wordsToSearch) )
-        context['query'] = query
-        context['results'] = results
+            results = Object.objects.filter(Q(public=True) & wordsToSearch)
+
+        paginator = Paginator(results, 10)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        context['results'] = page_obj
 
     return render(request, 'main/home.html', context)
+
+# def Home(request):
+#     context = {}
+#     if request.method == 'POST':
+#         query = request.POST.get('query')
+#         words = query.split(' ')
+#         wordsToSearch = reduce(operator.or_, (Q(name__icontains=w) for w in words))
+        
+#         if request.user.is_superuser:
+#             # results = Object.objects.filter(Q(name__icontains=query))
+#             results = Object.objects.filter(wordsToSearch)
+#         else:
+#             results = Object.objects.filter( Q(public=1) & Q(wordsToSearch) )
+
+#         paginator = Paginator(results, 1)
+#         page_number = request.GET.get('page')
+#         page_obj = paginator.get_page(page_number)
+
+#         context['query'] = query
+#         context['results'] = page_obj
+
+#     return render(request, 'main/home.html', context)
 
 @login_required
 def Details(request, id):
@@ -60,5 +87,10 @@ def Details(request, id):
 
 @login_required
 def MySearches(request):
-    searches = ViewLog.objects.filter(user=request.user)
-    return render(request, 'main/mysearches.html', { 'searches':searches })
+    all_searches = ViewLog.objects.filter(user=request.user).order_by('-last_seen')
+    paginator = Paginator(all_searches, 10)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'main/mysearches.html', { 'page_obj': page_obj })
